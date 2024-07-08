7a173e5bacc2c964a80799ad9dbf1ee27bc10408
bianca <biancajoanalopez@gmail.com>
2024-07-08 11:19:54 -0300

    Agregue un boton para insertar el nombre de un artista

--- Musica/cantante.py
+++ Musica/cantante.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, app, render_template
+from flask import Blueprint, app, redirect, render_template, request, url_for
 
 from . import db
 
@@ -38,4 +38,22 @@ def detalle(id):
     pagina = render_template('detalle_cantante.html',
                              artista =cantante,
                              albums = lista_de_albums)
-    return pagina
+    return pagina 
+
+@bp.route("/new", methods=("GET", "POST"))
+def nuevo():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+
+        con = db.get_db()
+        consulta = """ INSERT INTO cantante(firts_name, last_name)
+                       VALUES (?, ?) 
+                   """
+        con.execute(consulta, (first_name, last_name))
+        con.commit()
+        return redirect(url_for('cantante.cantantes'))
+    else:
+        pagina = render_template('nuevo_cantante.html')
+        return pagina
+        
